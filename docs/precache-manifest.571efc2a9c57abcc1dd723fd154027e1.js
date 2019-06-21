5de4c381a92562f5ae61420a4efae4a872073bf3
wuchong <2374175164@qq.com>
2019-06-21 10:40:01 +0800

    docs

---
+++ docs/precache-manifest.571efc2a9c57abcc1dd723fd154027e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f84525ba9c925066289c6a6d444a4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "350d07fc5954419cd790",
+    "url": "/static/css/2.400ebc83.chunk.css"
+  },
+  {
+    "revision": "bc13427743573bee8cbc",
+    "url": "/static/css/main.624fd554.chunk.css"
+  },
+  {
+    "revision": "350d07fc5954419cd790",
+    "url": "/static/js/2.8beea6fb.chunk.js"
+  },
+  {
+    "revision": "bc13427743573bee8cbc",
+    "url": "/static/js/main.afc462f5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
