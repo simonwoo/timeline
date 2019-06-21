645e541cef4f4bf5401de8764cc4c6115c52571c
wuchong <2374175164@qq.com>
2019-06-21 10:38:42 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Timeline, TimelineItem } from 'vertical-timeline-component-for-react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div class="icon avatar" />
+        <div class="name-and-title">
+          <span class="name">WU CHONG</span>
+          <span class="title">Full stack web developer</span>
+        </div>
+      </header>
+      <Timeline lineColor={'#ddd'} className="app_timeline">
+        <TimelineItem
+          key="001"
+          dateText="2015.03 - 2017.01"
+          style={{ color: '#e86971' }}
+        >
+          <h3><a href="https://restlet.com/" style={{textDecoration: 'none', color: "#333"}}>Restlet</a></h3>
+          <h4>前端开发工程师</h4>
+          <div style={{margin: "20px 0"}}>
+            <a href="https://studio.restlet.com/" style={{textDecoration:'none', color: "#888"}}>Restlet Studio</a>
+            <img src="/restlet-studio.png" style={{width: "100%", paddingTop: 10}}/>
+          </div>
+
+          <div style={{margin: "20px 0"}}>
+            <a href="https://restlet.com/modules/client/" style={{textDecoration:'none', color: "#888"}}>Restlet Client</a>
+            <img src="/restlet-client.png" style={{width: "100%", paddingTop: 10}}/>
+          </div>
+
+          <div style={{margin: "20px 0"}}>
+            <a href="https://restlet.com/modules/cloud/" style={{textDecoration:'none', color: "#888"}}>Restlet Cloud</a>
+            <img src="/restlet-cloud.png" style={{width: "100%", paddingTop: 10}}/>
+          </div>
+        </TimelineItem>
+
+        <TimelineItem
+          key="002"
+          dateText="2017.02 - 2018.06"
+          style={{ color: '#e86971' }}
+        >
+          <h3>阿里巴巴大鱼号</h3>
+          <h4>前端开发工程师</h4>
+          <div style={{margin: "20px 0"}}>
+            <a href="https://mp.dayu.com/" style={{textDecoration:'none', color: "#888"}}>大鱼号（PC端和Mobile端）</a>
+            <img src="/dayu.png" style={{width: "100%", paddingTop: 10}}/>
+          </div>
+          <div style={{margin: "20px 0"}}>
+            <a href="/" style={{textDecoration:'none', color: "#888"}}>大鱼号运营平台</a>
+          </div>
+          <div style={{margin: "20px 0"}}>
+            <a href="/" style={{textDecoration:'none', color: "#888"}}>大鱼号C端</a>
+
+          </div>
+        </TimelineItem>
+        <TimelineItem
+          key="003"
+          dateText="2018.06 - 至今"
+          style={{ color: '#e86971' }}
+        >
+          <h3>广发证券</h3>
+          <h4>前端开发leader</h4>
+          <div style={{margin: "20px 0"}}>
+            <a href="/" style={{textDecoration:'none', color: "#888"}}>投研平台</a>
+          </div>
+        </TimelineItem>
+      </Timeline>
+    </div>
+  );
+}
+
+export default App;
